5fce5b3fcea83445b32064024d5f9be8ccd39481
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-13 19:54:24 -0400

    Added express

--- db/db.js
+++ db/db.js
@@ -1,6 +1,7 @@
 
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 
 
 var app = express();
@@ -8,16 +9,24 @@ var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+
+var notes = [];
 
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/add", function (req, res) {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
+
+app.get("/api/notes", function (req, res) {
+    return res.json(notes);
+  });
