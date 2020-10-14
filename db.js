4d2c1875a2d3ed36370613caf8f90df3503cbe3f
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-13 20:39:38 -0400

    Updated db.js path files

--- db.js
+++ db.js
@@ -1,49 +1,53 @@
-
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
 let db = require("./db.json");
 
-
 var app = express();
-var PORT = 3000;
 var PORT = process.env.PORT || 3000;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 
-var notes = [];
+var note = [];
 
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/add", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT " + PORT);
 });
+
 
 app.get("/api/notes", function (req, res) {
-    return res.json(notes);
-    res.json(db);
+  res.json(db);
 });
 
-app.get("/api/notes/:title", function (req, res) {
-    fs.writeFileSync("db.json").push(newNote);
-
-
-});
 
 app.post("/api/notes", function (req, res) {
-    var newNote = req.body;
+  var newNote = req.body;
+  req.body.id = Math.floor(Math.random() * 9999) + 1;
+  note.push(newNote);
+  res.json(newNote);
+  var json = JSON.stringify(note);
+  fs.writeFileSync("db.json", json, "utf8");
+  console.log(req.body.id);
+});
 
-    var json = JSON.stringify(newNote);
 
-    fs.writeFileSync("db.json", json, "utf8");
+app.delete("/api/notes/:id", function (req, res) {
+  const id = req.params.id;
+  const deletedNote = db.splice(id);
+  var json = JSON.stringify(note);
+  fs.writeFileSync("db.json", json, "utf8");
+  res.json(deletedNote);
 });
