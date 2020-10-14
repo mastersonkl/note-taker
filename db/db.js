1582db84860677221fac1c16bd36f491482bc953
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-13 20:13:54 -0400

    Added require db.json

--- db/db.js
+++ db/db.js
@@ -2,10 +2,12 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
+let db = require("./db.json");
 
 
 var app = express();
 var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,17 +18,32 @@ var notes = [];
 
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/add", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT " + PORT);
 });
 
 app.get("/api/notes", function (req, res) {
     return res.json(notes);
-  });
+    res.json(db);
+});
+
+app.get("/api/notes/:title", function (req, res) {
+    fs.writeFileSync("db.json").push(newNote);
+
+
+});
+
+app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
+
+    var json = JSON.stringify(newNote);
+
+    fs.writeFileSync("db.json", json, "utf8");
+});
