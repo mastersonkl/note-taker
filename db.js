49841cc2e133d92c013bb40de0e29c296a5a0369
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-15 18:27:33 -0400

    Updated db.json

--- db.js
+++ db.js
@@ -14,28 +14,27 @@ app.use(express.static("public"));
 
 var note = [];
 
+//routes 
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
 
+//display index page
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+//display notes page
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
-});
-
-
+//display all notes 
 app.get("/api/notes", function (req, res) {
   res.json(db);
 });
 
-
+//create new note
 app.post("/api/notes", function (req, res) {
   var newNote = req.body;
   req.body.id = Math.floor(Math.random() * 9999) + 1;
@@ -46,7 +45,7 @@ app.post("/api/notes", function (req, res) {
   console.log(req.body.id);
 });
 
-
+//delete note
 app.delete("/api/notes/:id", function (req, res) {
   const id = req.params.id;
   const deletedNote = db.splice(id);
