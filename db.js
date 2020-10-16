2944e8d75594e1cad726fea32df4a8f9c116f1f1
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-15 20:47:59 -0400

    Updated stylespath

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
-let db = require('./db.json');
+var db = require('./db.json');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
