9d117e15084d9387124fe2fdbdcfed64d8b4d7b3
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-15 20:34:48 -0400

    updated routes

--- db.js
+++ db.js
@@ -16,27 +16,25 @@ var note = [];
 
 //routes 
 
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
 
-//display index page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-//display notes page
-app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-
-//display all notes 
-app.get("/api/notes", function(req, res) {
+// displays notes
+app.get("/api/notes", function (req, res) {
   res.json(db);
 });
 
-//create new note
-app.post("/api/notes", function(req, res) {
+// create new notes 
+app.post("/api/notes", function (req, res) {
   var newNote = req.body;
   req.body.id = Math.floor(Math.random() * 9999) + 1;
   note.push(newNote);
@@ -46,12 +44,11 @@ app.post("/api/notes", function(req, res) {
   console.log(req.body.id);
 });
 
-//delete note
-app.delete("/api/notes/:id", function(req, res) {
+// deletes notes
+app.delete("/api/notes/:id", function (req, res) {
   const id = req.params.id;
   const deletedNote = db.splice(id);
   var json = JSON.stringify(note);
   fs.writeFileSync("db.json", json, "utf8");
   res.json(deletedNote);
 });
-
