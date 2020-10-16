84340c321c750b1c2c32dff095ff476e8fbf2bd1
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-16 15:39:58 -0400

    Fixed functionin db.js to save notes

--- db.js
+++ db.js
@@ -3,8 +3,9 @@ var path = require('path');
 var fs = require('fs');
 var db = require('./db.json');
 
-var app = express();
+
 var PORT = process.env.PORT || 3000;
+var app = express();
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -12,43 +13,84 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
-var note = [];
+var notes = [];
 
-//routes 
-
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
 
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
 
-// displays notes
+// index page
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+//notes page
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// displays all notes
 app.get("/api/notes", function (req, res) {
-  res.json(db);
+  let json = getJson();
+  res.json(json);
 });
 
-// create new notes 
+// id for note
+const id = () => {
+  return Math.floor(Math.random() * 200);
+};
+
+// new note 
 app.post("/api/notes", function (req, res) {
+  const data = fs.readFileSync("./db.json", "utf8");
+
+  notes = JSON.parse(data);
+
+  // sets newNote equal to request.body
   var newNote = req.body;
-  req.body.id = Math.floor(Math.random() * 9999) + 1;
-  note.push(newNote);
+
+  // sets newNote.id equal to a randomly generated ID
+  newNote.id = id();
+
+  // pushes the note object into the notes array
+  notes.push(newNote);
+
+  // sends a json response with the notes to the API
   res.json(newNote);
-  var json = JSON.stringify(note);
+
+  const json = JSON.stringify(notes);
+
+
   fs.writeFileSync("db.json", json, "utf8");
-  console.log(req.body.id);
 });
 
-// deletes notes
+// delete note
 app.delete("/api/notes/:id", function (req, res) {
-  const id = req.params.id;
-  const deletedNote = db.splice(id);
-  var json = JSON.stringify(note);
-  fs.writeFileSync("db.json", json, "utf8");
-  res.json(deletedNote);
+  notes = fs.readFileSync("./db.json", "utf-8");
+  notes = JSON.parse(notes)
+
+
+  const deletedNote = req.params.id;
+
+
+  notes = notes.filter((note) => {
+    return note.id != deletedNote;
+  });
+
+
+  const permaNotes = JSON.stringify(notes);
+
+  fs.writeFileSync("./db.json", permaNotes, "utf8", (err) => {
+    if (err) throw err;
+  });
+
+
+  res.json(permaNotes);
 });
+
+function getJson() {
+  let data = fs.readFileSync(__dirname + "/db.json");
+  let json = JSON.parse(data);
+  return json;
+}
